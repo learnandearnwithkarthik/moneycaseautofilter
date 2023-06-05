bd7bc1d19018ef0277c3e05c1591ce3c86817c23
learnandearnwithkarthik <125831794+learnandearnwithkarthik@users.noreply.github.com>
2023-06-06 00:19:14 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -11,14 +11,14 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [5736579519, 'admin123', 98765432]
+CHANNELS = [-1001747872638, -100987654321, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'GreyMatter'
+DATABASE_URI = "mongodb+srv://devilbot:Rajbot@cluster0.ldrbggy.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'Cluster0'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 
